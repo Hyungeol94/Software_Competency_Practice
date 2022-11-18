2ebd1e9ef4723ba1a49de159650738d2d6cff80a
Hyungeol94 <55428818+Hyungeol94@users.noreply.github.com>
2022-11-18 12:05:51 +0900

    20221117 무한문자열 풀이입니다.

--- 20221117 무한문자열/무한문자열.py
+++ 20221117 무한문자열/무한문자열.py
@@ -1,28 +1,24 @@
-#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV134DPqAA8CFAYh
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AYP5JmsqcngDFATW
 
 def compute():
-    building_count = int(input());
-    building_list = list(map(int, input().split()));
-    #조망권이 확보된 세대 수를 구하기;
-    #각 빌딩의 최대 높이는 255
+    (a, b) = map(str, input().split(' '));
 
-    ##조망권 구하기
-    sum = 0;
-    for i in range(2, building_count-2):
-        building_height = building_list[i];
-        previous_height = max(building_list[i-2], building_list[i-1]);
-        next_height = max(building_list[i+1], building_list[i+2]);
+    first_string_length = len(a);
+    second_string_length = len(b);
 
-        compare_height = max(previous_height, next_height);
-        if (building_height>compare_height):
-            sum += building_height-compare_height;
+    new_a = a*second_string_length;
+    new_b = b*first_string_length;
 
-    return sum;
+    if (new_a==new_b):
+        return True;
+    else:
+        return False;
+
 
 if __name__ == '__main__':
-    for i in range(10):
-        answer = compute();
-        print("#"+str(i+1)+" "+str(answer));
-
-
-
+    n = int(input());
+    for i in range(1, n+1):
+        if (compute()):
+            print('#'+str(i)+' yes');
+        else:
+            print('#' + str(i) + ' no');
