432ec38bd43c71f2906b89761941c81eeb886f6d
Hyungeol94 <ekfrhkquf12@naver.com>
2023-02-16 20:26:07 +0900

    코딩마스터스 5434. 비밀번호 찾기

---
+++ 20230216 모두의 코딩/소스.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+    import sys
+    a = (sys.stdin.readline())
+    a = ''.join(a.split())
+    
+    index = -1
+    for i, letter in enumerate(a):
+        if letter == 'c':
+            index = i
+            break;
+
+    for i in (a[:index+1]):
+        if i == a[:index+1]:
+            print(i, end = '')
+            break
+        print(i, end = ' ')
