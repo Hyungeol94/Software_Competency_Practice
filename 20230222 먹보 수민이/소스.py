6e0d6671e264de7a2c184c678264fa463530b7b4
Hyungeol94 <ekfrhkquf12@naver.com>
2023-02-22 21:43:40 +0900

    feat: update_satisfaction

--- 20230222 먹보 수민이/소스.py
+++ 20230222 먹보 수민이/소스.py
@@ -1,6 +1,17 @@
 #https://coding-ai.aivle-edu.kr/Question/5394/View/1?fromPage=5
 import sys
-
+def update_satisfaction(data, check):
+    for i, [location, satisfaction, actual_satisfaction] in enumerate(data):
+        count = 0
+        for j in range(satisfaction):
+            if check[location+j-1] == True:
+                count += 1
+        actual_satisfaction = satisfaction - count
+        data[i] = [location, satisfaction, actual_satisfaction]
+        
+    data.sort(key = lambda a: (-a[2], a[0], a[1]))
+                
+        
 N = int(sys.stdin.readline())
 data = []
 for _ in range(N):
@@ -17,21 +28,30 @@ for i, [location, satisfaction] in enumerate(data):
         satisfaction = max(0, Destination-location)
     data[i] = [location, satisfaction]
 
+
+#actual_satisfaction 지표 만들기
+for i, datum in enumerate(data):
+    data[i].append(datum[1])
+    
+data.sort(key = lambda a: (-a[2], a[0]))
 #print(data)
-data.sort(key = lambda a: (-a[1], a[0]))
 
 check = [False]*Destination
 
 #포만감 많은 편의점 먼저 선택하기
 
-count = 0 
-for location, satisfaction in data:
+count = 0
+while(data):
+    #print(data)
+    (location, satisfaction, actual_satisfaction) = data.pop(0)
     if check.count(False)<=P:
         break
-    for i in range(satisfaction+1):        
+    for i in range(satisfaction):        
         check[location+i-1] = True
     count += 1
-    
+    #만족감 업데이트 해주기
+    update_satisfaction(data, check)
+       
 if check.count(False) <= P:
     print(count)
 else:
