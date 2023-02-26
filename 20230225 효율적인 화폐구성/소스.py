a4f10567b6d47aff67dd095e402995165da04ac7
Hyungeol94 <ekfrhkquf12@naver.com>
2023-02-26 16:08:21 +0900

    refactor: backtracking

--- 20230225 효율적인 화폐구성/소스.py
+++ 20230225 효율적인 화폐구성/소스.py
@@ -1,29 +1,41 @@
 import sys
 
+
 def dfs(N,
         M,
         point,
         depth):
-    global min_depth
-    if M <= point:
+    global min_depth, found
+    if M == point:
+        min_depth = depth
+        found = True
+        return
+
+    if M < point:
         min_depth = min(min_depth, depth)
         return
 
     else:
         for i in range(N):
-            point += data[i]
-            dfs(N, M, point, depth+1)
-            point -= data[i]
+            if not found:
+                point += data[i]
+                dfs(N, M, point, depth + 1)
+                point -= data[i]
 
 
 global min_depth
 min_depth = 10001
+found = False
 
 (N, M) = list(map(int, sys.stdin.readline().split()))
 data = []
 for _ in range(N):
     data.append(int(sys.stdin.readline()))
 
-dfs(N, M, 0, 0)
-print(min_depth)
+data.sort(reverse=True)
 
+dfs(N, M, 0, 0)
+if not found:
+    print(-1)
+else:
+    print(min_depth)
