6f6c20c9747b76d177034f606cc59a186ec4baf4
Hyungeol94 <ekfrhkquf12@naver.com>
2023-03-08 21:55:10 +0900

    4th trial: fail

--- 20230306 태국 택시/소스.py
+++ 20230306 태국 택시/소스.py
@@ -1,31 +1,36 @@
-def dfs(N, depth):
-    global Connected
-    if depth == N-1:
+global visit_count
+visit_count = 1
+
+def dfs(N):
+    global Connected, visit_count
+    if visit_count == N:
         Connected = True
         return True
 
     else:
         node = my_stack.pop()
-        for i in matrix[node]:
-            if not check[i]:
+        for i in matrix[node-1]:
+            if not check[i-1]:
                 my_stack.append(i)
-                check[i] = True
-                return dfs(N, depth+1)
+                check[i-1] = True
+                visit_count += 1
+                dfs(N)
 
 
-def test_dfs(N, depth):
+def test_dfs(N):
     global Connected
-    if depth == N-1:
+    if visit_count == N:
         Connected = True
         return True
 
     else:
         node = my_stack.pop()
-        for i in test_matrix[node]:
-            if not check[i]:
+        for i in test_matrix[node-1]:
+            if not check[i-1]:
                 my_stack.append(i)
-                check[i] = True
-                return test_dfs(N, depth+1)
+                visit_count += 1
+                check[i-1] = True
+                return test_dfs(N)
 
 import sys
 
@@ -51,37 +56,41 @@ my_stack = []
 visited = [False]*N
 check = [False]*N
 
+
 while not Connected and not my_queue.empty():
-    [cost, node1, node2] = my_queue.get()
-    matrix[node1 - 1].append(node2 - 1)
-    matrix[node2 - 1].append(node1 - 1)
+    [fee, node1, node2] = my_queue.get()
+    matrix[node1 - 1].append(node2)
+    matrix[node2 - 1].append(node1)
 
     #둘 중 하나라도 not visited 여야 함
     if (not visited[node1 - 1] or not visited[node2 - 1]) and not Connected:
         visited[node1 - 1] = True
         visited[node2 - 1] = True
-        total_fee += cost
+        total_fee += fee
     #모든 노드가 다 방문되었지만, 모든 노드가 다 연결되어 있지 않았을 수도 있을 때
     #dfs로, 연결되어 있는지를 확인해야 함
 
     if not (False in visited):
-        my_stack.append(0)
-        dfs(N, 0)
+        my_stack.append(1)
+        check[0] = True
+        dfs(N)
         check = [False] * N
         break
 
 from copy import deepcopy
 while not Connected and not my_queue.empty():
         # dfs해서 연결되었는지 체크하기
-        [cost, node1, node2] = my_queue.get()
-        matrix[node1 - 1].append(node2 - 1)
-        matrix[node2 - 1].append(node1 - 1)
+        visit_count = 1
+        [fee, node1, node2] = my_queue.get()
+        matrix[node1 - 1].append(node2)
+        matrix[node2 - 1].append(node1)
         my_stack.append(0)
-        dfs(N, 0)
+        check[0] = True
+        dfs(N)
         check = [False] * N
 
         if Connected:
-            total_fee += cost
+            total_fee += fee
             break
         # 다 연결하지 못해도, 두 개의 세트를 연결하기만 했어도 택시로서 가치있다고 인정해야 함
         # 두 세트가 연결되면 depth가 달라질 것이다. -> depth를 셀 수가 없음
@@ -92,33 +101,37 @@ while not Connected and not my_queue.empty():
         matrix[node1 - 1].pop()
         matrix[node2 - 1].pop()
 
-        i = 0
+        i = 1
         count = 0
-        while False in check:
-            if not check[i]:
+        while False in check:    #####무한루프 도는구간
+            visit_count = 1
+            if not check[i-1]:    #####무한루프 도는구간
                 count += 1
                 my_stack.append(i)
-                dfs(N, i)
+                dfs(N)
+            i += 1
         check = [False] * N
 
-        i = 0
+        i = 1
         test_count = 0
         global test_matrix
-        test_matrix = matrix.deepcopy()
-        test_matrix[node1 - 1].append(node2 - 1)
-        test_matrix[node2 - 1].append(node1 - 1)
+        test_matrix = deepcopy(matrix)
+        test_matrix[node1 - 1].append(node2)
+        test_matrix[node2 - 1].append(node1)
 
         while False in check:
-            if not check[i]:
+            visit_count = 1
+            if not check[i-1]:
                 test_count += 1
                 my_stack.append(i)
-                test_dfs(N, i)
+                test_dfs(N)
+            i += 1
         check = [False] * N
 
         if count != test_count:
-            matrix[node1 - 1].append(node2 - 1)
-            matrix[node2 - 1].append(node1 - 1)
-            total_fee += cost
+            matrix[node1 - 1].append(node2)
+            matrix[node2 - 1].append(node1)
+            total_fee += fee
 
 
 print(total_fee)
