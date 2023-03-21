a51845e8aa8a8ad26dc52b5e757e6e472fcdf17e
Hyungeol94 <ekfrhkquf12@naver.com>
2023-03-21 22:38:26 +0900

    refactor: 시간복잡도 O(NQ)로 개선 및 PyPy3로 재제출

--- 20230312 MooTube(Silver)/소스.py
+++ 20230312 MooTube(Silver)/소스.py
@@ -1,20 +1,20 @@
 #https://www.acmicpc.net/problem/15591
-global usado
-usado = 1000000001
 from collections import deque
 
-def bfs(my_queue, v, matrix, visited):
+def bfs(my_queue, matrix, visited, k):
     global usado
+    found = False
+    count = 0
     while my_queue:
         (node, pseudo_usado) = my_queue.popleft()
-        if node == v:
-            usado = min(pseudo_usado, usado)
-
-        else:
-            for (i, r) in matrix[node - 1]:
-                if not visited[i-1]:
-                    my_queue.append([i, min(pseudo_usado, r)])
-                    visited[i-1] = True
+        if (k <= pseudo_usado) and (pseudo_usado != 1000000001):
+          #  print(node, k)
+            count += 1
+        for (i, r) in matrix[node - 1]:
+            if not visited[i-1]:
+                my_queue.append([i, min(pseudo_usado, r)])
+                visited[i-1] = True
+    return count
 
 
 def calculate(k, v, matrix):
@@ -23,19 +23,16 @@ def calculate(k, v, matrix):
     count = 0
     my_queue = deque()
     visited = [False]*node_number
-    for i in range(1, node_number+1):
-        #(연결되어 있다면) i에서 v까지의 usado를 계산하기
-        usado = 1000000001
-        pseudo_usado = usado
-        my_queue.append([i, pseudo_usado])
-        bfs(my_queue, v,matrix, visited)
-        visited = [False] * node_number
-        if k <= usado and (usado != 1000000001):
-            count += 1
+    my_queue = deque()
+    usado = 1000000001
+    pseudo_usado = usado
+    my_queue.append([v, pseudo_usado])
+    visited[v-1] = True
+    count = bfs(my_queue, matrix, visited, k)
     return count
 
+
 (N, Q) = list(map(int, input().split()))
-global matrix
 matrix = []
 for _ in range(N):
     matrix.append([])
