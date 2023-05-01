28857c1405bb6b99ec540a15683e84e67341d085
Hyungeol94 <ekfrhkquf12@naver.com>
2023-05-01 12:17:49 +0900

    itertools 활용해 재구현

--- 20230501 무차별 대입 공격/소스.py
+++ 20230501 무차별 대입 공격/소스.py
@@ -1,19 +1,11 @@
 import sys
-
-def dfs(depth, my_stack, max_depth, char_string):
-    if depth == max_depth:
-        print(''.join(my_stack))
-
-    else:
-        for i in char_string:
-            my_stack.append(i)
-            dfs(depth+1, my_stack, max_depth, char_string)
-            my_stack.pop()
+import itertools
 
 num_char, max_depth = list(map(int, sys.stdin.readline().split()))
 char_string = sys.stdin.readline().strip()
 char_list = [char for char in char_string]
 char_list.sort()
 char_string = ''.join(char_list)
-my_stack =[]
-dfs(0, my_stack, max_depth, char_string)
+#print(char_list)
+for x in itertools.product(char_list, repeat=max_depth):
+    print(''.join(list(x)))
