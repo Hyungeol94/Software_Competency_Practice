6519a948e1f9119a5b90e2035f30bc5cd1147e2c
Hyungeol94 <ekfrhkquf12@naver.com>
2023-05-11 00:10:57 +0900

    4th trial: case 2,5,6 fail

--- 20230508 어묵과 파르페/소스.py
+++ 20230508 어묵과 파르페/소스.py
@@ -1,5 +1,6 @@
 import sys
 import copy
+from collections import deque
 
 def search(char_list):
     if 'F' not in char_list:
@@ -20,16 +21,30 @@ def search(char_list):
 
     sub_char_list = copy.copy(char_list)
     if left_parfait_count > abs(right_parfait_count):
-        sub_char_list = sub_char_list[:j]
+        while sub_char_list and sub_char_list[-1] == 'P':
+            sub_char_list.pop()
         while sub_char_list and sub_char_list[-1] == 'F':
             sub_char_list.pop()
         return abs(right_parfait_count) + search(sub_char_list)
 
-    else:
-        sub_char_list = sub_char_list[i:]
+    elif left_parfait_count < abs(right_parfait_count):
+        while sub_char_list and sub_char_list[0] == 'P':
+            sub_char_list.popleft()
         while sub_char_list and sub_char_list[0] == 'F':
-            sub_char_list.pop(0)
+            sub_char_list.popleft()
         return left_parfait_count + search(sub_char_list)
 
-char_list = list(sys.stdin.readline().strip())
+    else:
+        second_substring = copy.copy(char_list)
+        while sub_char_list and sub_char_list[-1] == 'P':
+            sub_char_list.pop()
+        while sub_char_list and sub_char_list[-1] == 'F':
+            sub_char_list.pop()
+        while sub_char_list and second_substring[0] == 'P':
+            second_substring.popleft()
+        while sub_char_list and second_substring[0] == 'F':
+            second_substring.popleft()
+        return min(left_parfait_count + search(second_substring), abs(right_parfait_count) + search(sub_char_list))
+
+char_list = deque(sys.stdin.readline().strip())
 print(search(char_list))
