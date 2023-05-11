1f95cf7a0c2b49db580c8d486b52aef90a1f46b0
Hyungeol94 <ekfrhkquf12@naver.com>
2023-05-11 23:15:35 +0900

    1st trial: union-find implementation - wrong

---
+++ 20230511 트리 의심하기/소스.py
@@ -0,0 +1,41 @@
+import sys
+
+
+def union(origin, destination, parents):
+    #weighted union
+    origin_root = find(origin, parents);
+    destination_root = find(destination, parents);
+    parents[destination_root] = origin_root
+
+
+def find(node, parents):
+    #collapsing find
+    i = node
+    if parents[i]!= i:
+        parents[i] = find(parents[i], parents)
+    return parents[i];
+
+N = int(sys.stdin.readline().strip())
+data = []
+parents = [i for i in range(N+1)]
+A, B, C = 'NO', 'NO', 'NO'
+num_components = N
+
+for _ in range(N-1):
+    origin, destination = list(map(int, sys.stdin.readline().split()))
+    if origin == destination:
+        B = "YES"
+    if origin not in range(1, N+1) or destination not in range(1, N+1):
+        C = "YES"
+    data.append((origin, destination))
+    if find(origin, parents) != find(destination, parents):
+        union(origin, destination, parents)
+        num_components -= 1
+if num_components != 1:
+    A = "YES"
+
+print(A)
+print(B)
+print(C)
+
+
