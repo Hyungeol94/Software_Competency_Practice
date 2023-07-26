cf7e9e13e497674d473c53ad53ee29f490b0eb2a
Hyungeol Lee <hyungeol@Hyungeolui-MacBookAir.local>
2023-07-26 15:53:46 +0900

    3rd trial - success

--- 20230725 두 원 사이의 정수 쌍/소스.py
+++ 20230725 두 원 사이의 정수 쌍/소스.py
@@ -5,20 +5,18 @@ def get_y_count(x, r1, r2):
     count = 0    
     if r1**2-x**2 > 0:
         start, end = math.ceil(math.sqrt(r1**2-x**2)), math.floor(math.sqrt(r2**2-x**2))+1
-        for _ in range(start, end):
-            count += 1             
-        start, end = math.ceil(-math.sqrt(r2**2-x**2)), math.floor(-math.sqrt(r1**2-x**2))+1
-        for _ in range(start, end):
-            count += 1           
+        count += abs(end-start)*2        
+             
     else:
-        for _ in range(math.ceil(-math.sqrt(r2**2-x**2)), math.floor(math.sqrt(r2**2-x**2))+1):
-            count += 1           
+        start, end = math.ceil(-math.sqrt(r2**2-x**2)), math.floor(math.sqrt(r2**2-x**2))+1
+        count += abs(end-start)     
     return count
                    
 def solution(r1, r2):    
     count = 0
-    for x in range(-r2, r2+1):
-        count += get_y_count(x, r1, r2)
+    for x in range(1, r2+1):
+        count += get_y_count(x, r1, r2)*2
+    count += get_y_count(0, r1, r2)
     return count
         
 
