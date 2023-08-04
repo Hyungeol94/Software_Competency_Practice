feb0f30e71d2ef2ecd54f27ef0f7d44f4df06011
Hyungeol Lee <hyungeol@Hyungeolui-MacBookAir.local>
2023-08-04 14:00:53 +0900

    3rd trial using dp+binary search: timeout

--- 20230803 연속된 부분 수열의 합/소스.py
+++ 20230803 연속된 부분 수열의 합/소스.py
@@ -1,47 +1,54 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/178870
 
-def solution(sequence, k):    
-    matrix = []
+def get(x, y, matrix, sequence):
+    if matrix[x][y] == 0:
+        matrix[x][y] = get(x, y-1, matrix, sequence) + sequence[y]
+    return matrix[x][y]
+
+def find(start, end, k, matrix, sequence):
+    lux, luy = start
+    rdx, rdy = end
+    middle = (lux+rdx)//2, (luy+rdy)//2
+    x, y = middle
+    if get(x, y, matrix, sequence) == k:
+        return [x, y]
+    if (lux == x and luy == y) and (get(x, y, matrix, sequence) != k):
+        return False
+    return find(start, middle, k, matrix, sequence) if matrix[x][y] > k else find(middle, end, k, matrix, sequence)
+
+def search_value(matrix, value, k, sequence):
+    x, y = value
+    while(x != 0):
+        x-= 1
+        y-= 1
+    while(x != len(matrix)-1):
+        if get(x, y, matrix, sequence) == k:
+            return [x, y]
+        x += 1
+        y += 1
+
+
+
+
+def solution(sequence, k):
+    matrix = [[0 for _ in range(len(sequence))] for _ in range(len(sequence))]
     for i in range(len(sequence)):
-        matrix.append([0]*len(sequence))
         matrix[i][i] = sequence[i]
         if matrix[i][i] == k:
             return [i, i]
-    
+
     shift = 1
     while shift != len(sequence):
-        for i in range(len(sequence)):
-            start = i
-            end = start+shift
-            if end == len(sequence):
-                break            
-            matrix[start][end] = matrix[start][end-1]+sequence[end]
-            if matrix[start][end] == k:
-                return [start, end]
-        shift += 1 
-        
-            
-        
-            def solution(sequence, k):    
-    matrix = []
-    for i in range(len(sequence)):
-        matrix.append([0]*len(sequence))
-        matrix[i][i] = sequence[i]
-        if matrix[i][i] == k:
-            return [i, i]
-    
-    shift = 1
-    while shift != len(sequence):
-        for i in range(len(sequence)):
-            start = i
-            end = start+shift
-            if end == len(sequence):
-                break            
-            matrix[start][end] = matrix[start][end-1]+sequence[end]
-            if matrix[start][end] == k:
-                return [start, end]
-        shift += 1 
-        
-            
-        
-            
+        if shift > k:
+            break
+        start = (0, shift)
+        end = (len(sequence)-1-shift, len(sequence)-1)
+        value = find(start, end, k, matrix, sequence)
+        if value:
+            #return value
+            return search_value(matrix, value, k, sequence)
+        shift += 1
+
+
+
+
