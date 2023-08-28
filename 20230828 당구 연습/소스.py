10ffd613e48bbc4e55c6e5c4c9e64bd0e55055cf
Hyungeol Lee <hyungeol@Hyungeolui-MacBookAir.local>
2023-08-28 21:57:34 +0900

    프로그래머스 '당구 연습' 풀이입니다.

--- 20230828 당구 연습/소스.py
+++ 20230828 당구 연습/소스.py
@@ -3,40 +3,33 @@
 def calculate_distance(ball, m, n, startX, startY):
     targetX, targetY = ball
     distances = []
-    a,b,c,d, = startX, startY, targetX, targetY
+    a,b,c,d = startX, startY, targetX, targetY
     
     #좌반사
-    if startY!=targetY:
-        pointX, pointY =0, (startY*targetX+startX*targetY)/(startX+targetX)
-        left_distance = (((startX-pointX)**2+(startY-pointY)**2)**(0.5)+((pointX-targetX)**2+(pointY-targetY)**2)**(0.5))**2
-        distances.append(left_distance)
-    
+    if startY!=targetY or startX<targetX:
+        x, y = 0, (a*d+b*c)/(a+c)
+        distances.append((((a-x)**2+(b-y)**2)**0.5+((c-x)**2+(d-y)**2)**0.5)**2)
+
     #우반사
-    if startY!=targetY:
-        pointX, pointY =m, (startY*targetX+startX*targetY-(startY+targetY)*m)/(startX+targetX-2*m)
-        right_distance = (((startX-pointX)**2+(startY-pointY)**2)**(0.5)+((pointX-targetX)**2+(pointY-targetY)**2)**(0.5))**2
-        distances.append(right_distance)
+    if startY!=targetY or targetX<startX:
+        x, y = m, ((b+d)*m-(a*d+b*c))/(2*m-(a+c))
+        distances.append((((a-x)**2+(b-y)**2)**0.5+((c-x)**2+(d-y)**2)**0.5)**2)
     
     #상반사
-    if startX!=targetX:
-        pointX, pointY = (startY*targetX+startX*targetY)/(startY+targetY), 0
-        up_distance = (((startX-pointX)**2+(startY-pointY)**2)**(0.5)+((pointX-targetX)**2+(pointY-targetY)**2)**(0.5))**2
-        distances.append(up_distance)
-    
+    if startX!=targetX or targetY<startY:
+        x, y = (a*d+b*c-(a+c)*n)/(b+d-2*n), n
+        distances.append((((a-x)**2+(b-y)**2)**0.5+((c-x)**2+(d-y)**2)**0.5)**2)
+        
     #하반사
-    if startX!=targetX:
-        pointX, pointY = (startY*targetX+startX*targetY-(startX-targetX)*n)/(startY+targetY-2*n), n
-        down_distance = (((startX-pointX)**2+(startY-pointY)**2)**(0.5)+((pointX-targetX)**2+(pointY-targetY)**2)**(0.5))**2
-        distances.append(down_distance)
+    if startX!=targetX or startY<targetY:
+        x, y = (a*d+b*c)/(b+d), 0
+        distances.append((((a-x)**2+(b-y)**2)**0.5+((c-x)**2+(d-y)**2)**0.5)**2)
     return min(distances)
-     
     
-
 def solution(m, n, startX, startY, balls):
     #4가지 방법이 있을 것임
     answer = []
-    
     for ball in balls:
         answer.append(calculate_distance(ball, m, n, startX, startY))
     
-    return answer
+    return list(map(round, answer))
