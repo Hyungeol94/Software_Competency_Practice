c8e9b1076ff309995b6d6779fae26ffcc9f7d01c
Hyungeol Lee <hyungeol@Hyungeolui-MacBookAir.local>
2023-09-07 14:01:54 +0900

    프로그래머스 '연속된 부분 수열의 합' improved but edge cases exists

--- 20230803 연속된 부분 수열의 합/소스.py
+++ 20230803 연속된 부분 수열의 합/소스.py
@@ -1,40 +1,52 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/178870
-
-def get(x, y, matrix, sequence):
-    if matrix[x][y] == 0:
-        matrix[x][y] = get(x, y-1, matrix, sequence) + sequence[y]
-    return matrix[x][y]
+#https://school.programmers.co.kr/learn/courses/30/lessons/178870
 
 def solution(sequence, k):
-    matrix = []
-    for i in range(len(sequence)):
-        matrix.append([0] * len(sequence))
-        matrix[i][i] = sequence[i]
-        if matrix[i][i] == k:
-            return [i, i]
-
-    left = 0
+    t = 0
+    left = -1
     right = 0
-    candidates = []
-    while not(left == len(sequence)-1 and right == len(sequence)-1):
-        while right != len(sequence):
-            subsequence_sum = get(left, right, matrix, sequence)
-            if subsequence_sum < k:
-                right += 1
-            if subsequence_sum > k:
-                break
-            if subsequence_sum == k:
-                candidates.append([left, right])
-                break
+    right_max = len(sequence)-1
+    #10만번 딱 한 번 훑을 수 있어야 함
+    subsequence_sum = sequence[0]
+    command = 'expand'
+    answer = []
+    while True:
+        subsequence_sum -= sequence[left] if left >= 0 else 0
         left += 1
-        right = left
 
-    candidates.sort(key=lambda a: (abs(a[1]-a[0])))
-    
-    return candidates[0]
+        if right < left or left == len(sequence):
+            break
+
+        command = 'expand' if subsequence_sum < k else 'shrink'
+
+        #expand
+        if command == 'expand':
+            while True:
+                if k <= subsequence_sum:
+                    break
+                right += 1
+                if right == len(sequence):
+                    right = len(sequence)-1
+                    break
+                subsequence_sum += sequence[right]
+
+        #shrink
+        if command == 'shrink':
+            while True:
+                if subsequence_sum <= k:
+                    break
+                subsequence_sum -= sequence[right]
+                right -= 1
+
+        if subsequence_sum == k:
+            answer.append([left, right])
 
 
 
+    answer.sort(key = lambda a:(a[1]-a[0], a[0]))
+    #print(answer)
+    return answer[0]
 
-
+# print(solution([1, 2, 3, 4, 5], 7))
+# print(solution([1, 1, 1, 2, 3, 4, 5], 5))
+# print(solution([2, 2, 2, 2, 2], 6))
 
