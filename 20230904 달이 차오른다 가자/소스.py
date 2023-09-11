328bfc20b9b40a24a74d3d2745b7b0823239f996
Hyungeol Lee <hyungeol@Hyungeolui-MacBookAir.local>
2023-09-11 13:08:13 +0900

    4th trial- fail

--- 20230904 달이 차오른다 가자/소스.py
+++ 20230904 달이 차오른다 가자/소스.py
@@ -1,17 +1,16 @@
 #https://www.acmicpc.net/problem/1194
 
-
 from collections import deque
-import copy
+
 def passable(row, col, keys, matrix):
     if matrix[row][col].lower() in keys:
         return True
-    if matrix[row][col] in '01.abcdefghijklmnopqrstuvwxyz':
+    if matrix[row][col] in '01.abcdef':
         return True
     return False
 
 def found_new_key(mark, keys):
-    if mark in 'abcdefghijklmnopqrstuvwxyz' and not mark in keys:
+    if mark in 'abcdef' and not mark in keys:
         return True
     return False
 
@@ -24,56 +23,50 @@ def is_valid(next_row, next_col, matrix):
     return True
 
 
-def clear_visited(visited, row, col):
-    for i, r in enumerate(visited):
-        visited[i] = [False]*len(visited[0])
-    check_visited(row, col, visited)
-    return visited
-
-
-def check_visited(row, col, visited):
-    visited[row][col] = True
-
-def bfs(matrix, queue, result):
+def bfs(matrix, visited, queue, result):
     drs = [0, -1, 0, 1]
     dcs = [1, 0, -1, 0]
+
     while queue:
-        [row, col, keys, depth] = queue.popleft()
+        [row, col, depth, keys] = queue.popleft()
         if matrix[row][col] == '1':
             result[0] = min(result[0], depth)
-            continue
 
         if found_new_key(matrix[row][col], keys):
-            keys = keys+matrix[row][col]
-            clear_visited(visited, row, col)
-            queue = deque([[row, col, keys, depth]])
+            keys = sorted(keys+[matrix[row][col]])
+            visited[''.join(keys)] = []
+            for _ in range(len(matrix)):
+                visited[''.join(keys)].append([False]*len(matrix[0]))
+            visited[''.join(keys)][row][col] = True
 
         for dr, dc in zip(drs, dcs):
             next_row = row+dr
             next_col = col+dc
             if (is_valid(next_row, next_col, matrix)
                     and passable(next_row, next_col, keys, matrix)
-                    and not visited[next_row][next_col]):
-                check_visited(next_row, next_col, visited)
-                queue.append([next_row, next_col, keys, depth+1])
+                    and not visited[''.join(keys)][next_row][next_col]):
+                queue.append([next_row, next_col, depth+1, keys])
+                visited[''.join(keys)][next_row][next_col] = True
+
 
 n, m = map(int, input().split())
 matrix = []
-visited = []
 start = []
 end = []
-keys = ''
+keys = []
+visited = {}
+visited[''.join(keys)] = []
 for i in range(n):
     matrix.append(input().strip())
-    visited.append([False]*m)
+    visited[''.join(keys)].append([False]*m)
     for j, mark in enumerate(matrix[i]):
         if mark == '0':
             start = [i,j]
-            check_visited(i,j, visited)
+            visited[''.join(keys)][i][j] = True
 
-start.append(keys)
 start.append(0)
+start.append(keys)
 queue = deque([start])
 result = [25001]
-bfs(matrix, queue, result)
+bfs(matrix, visited, queue, result)
 print(result[0]) if result[0]!=25001 else print(-1)
