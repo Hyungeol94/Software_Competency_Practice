6dcb475d9149a0da28f4410628bfe58065fb8477
Hyungeol Lee <ekfrhkquf12@naver.com>
2023-10-05 23:54:41 +0900

    프로그래머스 '호텔 대실' 2nd trial- success

--- 20231005 호텔 대실/소스.py
+++ 20231005 호텔 대실/소스.py
@@ -1,40 +1,50 @@
-#https://school.programmers.co.kr/learn/courses/30/lessons/155651
 from collections import deque
+
 
 def minutes(time):
     hours = int(time[:2])
     minutes = int(time[-2:])
-    return hours*60+minutes
+    return hours * 60 + minutes
 
 
 def is_in_range(criteria, target):
-    if target[0]<criteria[1]:
+    if target[0] < criteria[1]:
         return True
     return False
 
 
-def solution(book_time):
-    #분으로 환산
-    book_time_converted = list(map(lambda a:
-        (minutes(a[0]), minutes(a[1])+10),
-        book_time))
-    
-    #마침시간을 중심으로 정렬하기
-    book_time_converted.sort(key = lambda a: a[1])
+def checkout_rooms(i, occupied_rooms):
+    updated_rooms = []
+    for j, [start_time, end_time] in enumerate(occupied_rooms):
+        if i != end_time:
+            updated_rooms.append(occupied_rooms[j])
+    return updated_rooms
 
-    
-    #최소 방의 수 구하기
+def checkin_rooms(i, occupied_rooms, book_time_converted):
+    for j, [start_time, end_time] in enumerate(book_time_converted):
+        if i == start_time:
+            occupied_rooms.append(book_time_converted[j])
+    return occupied_rooms
+
+
+def solution(book_time):
+    # 분으로 환산
+    book_time_converted = list(map(lambda a:
+                                   (minutes(a[0]), minutes(a[1]) + 10),
+                                   book_time))
+
+    # 시작시간을 중심으로 정렬하기
+    book_time_converted.sort(key=lambda a: a[0])
+
+    # 최소 방의 수 구하기
     book_time_converted = deque(book_time_converted)
+    i = 0
+    occupied_rooms = []
     room = 1
-    while book_time_converted:
-        criteria = book_time_converted.popleft()
-        i = 1
-        while book_time_converted:
-            target = book_time_converted[0]
-            if is_in_range(criteria, target):
-                i += 1
-                book_time_converted.popleft()
-            else:
-                break
-        room = max(room, i)
+    while i != minutes("24:00"):
+        occupied_rooms = checkout_rooms(i, occupied_rooms) if occupied_rooms else []
+        occupied_rooms = checkin_rooms(i, occupied_rooms, book_time_converted)
+        room = max(room, len(occupied_rooms)) if occupied_rooms else room
+        i += 1
+
     return room
