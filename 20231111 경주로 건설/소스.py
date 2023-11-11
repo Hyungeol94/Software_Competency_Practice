2fbeffaf355f757ab7dbe1f9d81d57868e59c5dd
Hyungeol Lee <ekfrhkquf12@naver.com>
2023-11-11 16:57:06 +0900

    3rd approach using 3D visit array - success

--- 20231111 경주로 건설/소스.py
+++ 20231111 경주로 건설/소스.py
@@ -1,49 +1,39 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/67259
 import heapq
-from copy import copy
-
 
 def isValid(y, x, n):
-    if 0 <= x <= n - 1 and 0 <= y <= n - 1:
-        return True
-    return False
-
-
-def markAsVisited(visited, row, col):
-    visited[row] = (visited[row] | (1 << col))
-
-
-def isVisited(visited, row, col):
-    if (visited[row] & (1 << col)):
-        return True
-    return False
-
+    return 0 <= x < n and 0 <= y < n
 
 def solution(board):
     n = len(board)
     dys = [0, 1, 0, -1]
     dxs = [1, 0, -1, 0]
     path_dirs = ['l', 'd', 'r', 'u']
-    visited = [0] * n
-    markAsVisited(visited, 0, 0)
 
-    pq = []  # 비용, 방향, 방문
+    visited = [[[float('inf')] * 4 for _ in range(n)] for _ in range(n)]  # Initialize a 3D array with infinity values
+    visited[0][0] = [0, 0, 0, 0]
+
+    pq = []  # cost, row, col, current direction
     for dy, dx, path_dir in zip(dys, dxs, path_dirs):
-        if isValid(dy, dx, n) and not board[dy][dx] == 1:
-            new_visited = copy(visited)
-            markAsVisited(new_visited, dy, dx)
-            heapq.heappush(pq, [100, path_dir, visited, dy, dx])
+        if isValid(dy, dx, n) and board[dy][dx] != 1:
+            visited[dy][dx][path_dirs.index(path_dir)] = 100
+            heapq.heappush(pq, (100, dy, dx, path_dir))
 
     while pq:
-        current_cost, current_path_dir, visited, row, col = heapq.heappop(pq)
+        current_cost, row, col, current_dir = heapq.heappop(pq)
         if row == n - 1 and col == n - 1:
             return current_cost
-        for dx, dy, path_dir in zip(dxs, dys, path_dirs):
-            next_row = row + dy
-            next_col = col + dx
-            cost = 100 if path_dir == current_path_dir else 600
-            if isValid(next_row, next_col, n) and not isVisited(visited, next_row, next_col) and not board[next_row][
-                                                                                                         next_col] == 1:
-                new_visited = copy(visited)
-                markAsVisited(new_visited, next_row, next_col)
-                heapq.heappush(pq, [current_cost + cost, path_dir, new_visited, next_row, next_col])
+
+        for i in range(4):
+            next_row = row + dys[i]
+            next_col = col + dxs[i]
+
+            if isValid(next_row, next_col, n) and board[next_row][next_col] != 1:
+                new_dir = path_dirs[i]
+                cost = 100 if new_dir == current_dir else 600
+
+                if current_cost + cost < visited[next_row][next_col][path_dirs.index(new_dir)]:
+                    visited[next_row][next_col][path_dirs.index(new_dir)] = current_cost + cost
+                    heapq.heappush(pq, (current_cost + cost, next_row, next_col, new_dir))
+
+    return -1  # No path found
