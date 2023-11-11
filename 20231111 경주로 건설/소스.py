c605c59daf5befbd8923bd42d7f0181d9f61cf70
Hyungeol Lee <ekfrhkquf12@naver.com>
2023-11-11 16:03:24 +0900

    2nd trial using Dijkstra -fail

--- 20231111 경주로 건설/소스.py
+++ 20231111 경주로 건설/소스.py
@@ -1,45 +1,49 @@
-#https://school.programmers.co.kr/learn/courses/30/lessons/67259	
+# https://school.programmers.co.kr/learn/courses/30/lessons/67259
 import heapq
-from copy import deepcopy
+from copy import copy
+
 
 def isValid(y, x, n):
-    if 0<=x<=n-1 and 0<=y<=n-1:
+    if 0 <= x <= n - 1 and 0 <= y <= n - 1:
         return True
     return False
 
+
 def markAsVisited(visited, row, col):
-    visited[row]  = (visited[row] | (1 << col))
-    
-def isVisited(visited, row, col): 
+    visited[row] = (visited[row] | (1 << col))
+
+
+def isVisited(visited, row, col):
     if (visited[row] & (1 << col)):
         return True
     return False
+
 
 def solution(board):
     n = len(board)
     dys = [0, 1, 0, -1]
     dxs = [1, 0, -1, 0]
     path_dirs = ['l', 'd', 'r', 'u']
-    visited = [0]*n
+    visited = [0] * n
     markAsVisited(visited, 0, 0)
-    
-    pq = [] # 비용, 방향, 방문
+
+    pq = []  # 비용, 방향, 방문
     for dy, dx, path_dir in zip(dys, dxs, path_dirs):
         if isValid(dy, dx, n) and not board[dy][dx] == 1:
-            new_visited = deepcopy(visited)
+            new_visited = copy(visited)
             markAsVisited(new_visited, dy, dx)
             heapq.heappush(pq, [100, path_dir, visited, dy, dx])
-    
+
     while pq:
-        current_cost, current_path_dir, visited, row, col= heapq.heappop(pq)
-        print(current_cost)
-        if row == n-1 and col == n-1:
+        current_cost, current_path_dir, visited, row, col = heapq.heappop(pq)
+        if row == n - 1 and col == n - 1:
             return current_cost
         for dx, dy, path_dir in zip(dxs, dys, path_dirs):
-            next_row = row+dy
-            next_col = col+dx
-            cost = 100 if path_dir == current_path_dir else 500
-            if isValid(next_row, next_col, n) and not isVisited(visited, next_row, next_col) and not board[next_row][next_col]==1:
-                new_visited = deepcopy(visited)
+            next_row = row + dy
+            next_col = col + dx
+            cost = 100 if path_dir == current_path_dir else 600
+            if isValid(next_row, next_col, n) and not isVisited(visited, next_row, next_col) and not board[next_row][
+                                                                                                         next_col] == 1:
+                new_visited = copy(visited)
                 markAsVisited(new_visited, next_row, next_col)
-                heapq.heappush(pq, [current_cost+cost, path_dir, visited, next_row, next_col])
+                heapq.heappush(pq, [current_cost + cost, path_dir, new_visited, next_row, next_col])
