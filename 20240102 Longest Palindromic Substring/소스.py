2e947d5885528d11609badfe05a5a929f4310ad8
Hyungeol Lee <ekfrhkquf12@naver.com>
2024-01-02 21:32:21 +0900

    4th trial-success

--- 20240102 Longest Palindromic Substring/소스.py
+++ 20240102 Longest Palindromic Substring/소스.py
@@ -10,21 +10,50 @@ class Solution:
             palindromeCheck[i][i + 1] = True
             palindrome = s[i:i+1]
 
+        k = 1
+        for j in range(0, limit-k):
+            start = j
+            end = j + k
+            palindromeCheck[start][end] = True if s[start] == s[end] else False
+            if (palindromeCheck[start][end]):
+                palindrome = s[start:end + 1]
+
         for k in range(1, limit): #added value
             for j in range(0, limit-k):
-                start_index = j
-                end_index = k
-                #print(j, j+k)
-                if len(s[j:j+k+1]) == 2:
-                    palindromeCheck[j][j + k] = True if s[j] == s[j + k] else False
-                else:
-                    palindromeCheck[j][j+k] = True if (palindromeCheck[j+1][j+k-1] and s[j] == s[j+k]) else False
-                if (palindromeCheck[j][j+k]):
-                     palindrome = s[j:j+k+1]
+                start = j
+                end = j+k
+                palindromeCheck[start][end] = True if (palindromeCheck[start+1][end-1] and s[start] == s[end]) else False
+                if (palindromeCheck[start][end]):
+                     palindrome = s[start:end+1]
         return palindrome
 
-#test = Solution()
-#print(test.longestPalindrome("abcba"))
-#print(test.longestPalindrome("cbbd"))
-#print(test.longestPalindrome("babad"))
+
+class Solution:
+    # palinedromeCheck[a][b]는 s[a:b]가 palindrome인지 여부를 담고 있음
+
+    def longestPalindrome(self, s: str) -> str:
+        palindrome = ''
+        limit = len(s)
+        palindromeCheck = [[False] * (limit+1) for _ in range(limit+1)]
+        for i in range(limit):
+            palindromeCheck[i][i] = True
+            palindromeCheck[i][i + 1] = True
+            palindrome = s[i:i+1]
+
+        k = 1
+        for j in range(0, limit-k):
+            start = j
+            end = j + k
+            palindromeCheck[start][end] = True if s[start] == s[end] else False
+            if (palindromeCheck[start][end]):
+                palindrome = s[start:end + 1]
+
+        for k in range(2, limit): #added value
+            for j in range(0, limit-k):
+                start = j
+                end = j+k
+                palindromeCheck[start][end] = True if (palindromeCheck[start+1][end-1] and s[start] == s[end]) else False
+                if (palindromeCheck[start][end]):
+                     palindrome = s[start:end+1]
+        return palindrome
 
