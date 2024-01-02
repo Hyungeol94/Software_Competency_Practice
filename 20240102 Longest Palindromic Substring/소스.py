cbadc96c0c56b1b111c7017f2e9a10ca0368bba6
Hyungeol Lee <ekfrhkquf12@naver.com>
2024-01-02 21:24:04 +0900

    3rd trial- timeout

--- 20240102 Longest Palindromic Substring/소스.py
+++ 20240102 Longest Palindromic Substring/소스.py
@@ -1,36 +1,30 @@
 class Solution:
     # palinedromeCheck[a][b]는 s[a:b]가 palindrome인지 여부를 담고 있음
-    palindromeCheck = [[False] * 1001 for _ in range(1001)]
-    for i in range(1000):
-        palindromeCheck[i][i] = True
-        palindromeCheck[i][i + 1] = True
-
-    def isPalindrome(self, s, start, end) -> bool:
-        if len(s[start:end]) == 1:
-            return True
-        if self.palindromeCheck[start + 1][end - 1] and s[start] == s[end - 1]:
-            self.palindromeCheck[start][end] = True
-            return True
-        else:
-            self.palindromeCheck[start][end] = False
-            return False
 
     def longestPalindrome(self, s: str) -> str:
         palindrome = ''
-        distance = 1
-        continue_searching = [i for i in range(0, len(s)+2)]
-        while distance != len(s) + 1:
-            start = 0
-            end = start + distance
-            while end <= len(s):
-                if self.isPalindrome(s, start, end):
-                    palindrome = s[start:end]
-                start += 1
-                end += 1
-            continue_searching[distance] = True
-            distance += 1
-            if 3 <= distance:
-                if not (continue_searching[distance-1] or continue_searching[distance-2]):
-                    break
+        limit = len(s)
+        palindromeCheck = [[False] * (limit+1) for _ in range(limit+1)]
+        for i in range(limit):
+            palindromeCheck[i][i] = True
+            palindromeCheck[i][i + 1] = True
+            palindrome = s[i:i+1]
+
+        for k in range(1, limit): #added value
+            for j in range(0, limit-k):
+                start_index = j
+                end_index = k
+                #print(j, j+k)
+                if len(s[j:j+k+1]) == 2:
+                    palindromeCheck[j][j + k] = True if s[j] == s[j + k] else False
+                else:
+                    palindromeCheck[j][j+k] = True if (palindromeCheck[j+1][j+k-1] and s[j] == s[j+k]) else False
+                if (palindromeCheck[j][j+k]):
+                     palindrome = s[j:j+k+1]
         return palindrome
+
+#test = Solution()
+#print(test.longestPalindrome("abcba"))
+#print(test.longestPalindrome("cbbd"))
+#print(test.longestPalindrome("babad"))
 
