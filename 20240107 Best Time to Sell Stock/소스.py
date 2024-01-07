0c15d50322f8aec932992366ed966ca26b2cb46a
Hyungeol Lee <ekfrhkquf12@naver.com>
2024-01-07 21:31:24 +0900

    LeetCode 'Best Time to Sell Stock' 2nd trial using 2-pointers: success

--- 20240107 Best Time to Sell Stock/소스.py
+++ 20240107 Best Time to Sell Stock/소스.py
@@ -1,10 +1,17 @@
 class Solution:
-    profit = -1
     def maxProfit(self, prices: List[int]) -> int:
-        for i, purchasePrice in enumerate(prices):
-            for j, sellingPrice in enumerate(prices[i:]):
-                self.profit = max(self.profit, sellingPrice-purchasePrice)
-        return self.profit
+        maxProfit = 0
+        left = 0
+        right = 1
+        while (right != len(prices)):
+            maxProfit = max(maxProfit, prices[right]-prices[left])
+            if prices[left] > prices[right]:
+                left = right
+            right += 1
+        return maxProfit
+            
             
                 
-    #사는 시점에서는 파는 시점이 미래여야 함
+
+
+        
