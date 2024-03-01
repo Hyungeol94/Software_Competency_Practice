1d0c13feed403dd2711b1c2ae51a6ad37262f884
Hyungeol Lee <ekfrhkquf12@naver.com>
2024-03-01 17:47:13 +0900

    LeetCode 'Maximum Length of Repeated Subarray' 답안 참고 풀이

--- 2024/20240301 Maximum Length of Repeated Subarray/소스.py
+++ 2024/20240301 Maximum Length of Repeated Subarray/소스.py
@@ -8,20 +8,48 @@ class Solution:
         # longeset common subsequence와는 구별됨
         n = len(nums1)
         m = len(nums2)
-        @lru_cache(maxsize = None)
-        def dp(i, j):
-            # base case
-            if i == n:
-                return 0
-            if j == m:
-                return 0
-            else: 
-                if nums1[i] == nums2[j]:
-                    return dp(i+1, j+1)+1
-                else:
-                    return 0
+        
+        dp = [[0]*(m+1) for _ in range(n+1)]
+        for j in range(m):
+            dp[n-1][j] = 1 if nums1[n-1] == nums2[j] else 0
+        
+        for i in range(n):
+            dp[i][m-1] = 1 if nums2[m-1] == nums1[i] else 0
+
+        for i in range(n-1, -1, -1):
+            for j in range(m-1, -1, -1):                
+                if nums1[i] == nums2[j]:                  
+                    dp[i][j] = dp[i+1][j+1]+1 
+       
         maxLen = 0
         for i in range(n):
-            for j in range(m):
-                maxLen = max(maxLen, dp(i, j))
+            maxLen = max(maxLen, max(dp[i]))
         return maxLen
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
