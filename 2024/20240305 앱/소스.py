67e917c2b8c11c1235d70f0e390b7cd7f707e193
Hyungeol Lee <ekfrhkquf12@naver.com>
2024-03-06 20:53:17 +0900

    BOJ '앱' 3rd trial- success

--- 2024/20240305 앱/소스.py
+++ 2024/20240305 앱/소스.py
@@ -1,44 +1,49 @@
 #https://www.acmicpc.net/problem/7579
 
-
-from functools import lru_cache
+import sys
+input = sys.stdin.readline
 class Solution():
     def minCost(self, n, k, memories, costs):
-        #메모리 크기별로 정렬하기
-        arr = list(zip(memories, costs)).sort(key=lambda a: a[0])
-        #sliding window 방식
         if n == 1:
-            return costs[0]
+            if k <= memories[0]:
+                return costs[0]
+            return float('inf')
 
-        left = 0
-        right = 1
-        sum = memories[0]+memories[1]
-        cost = costs[0]+costs[1]
+        dp = [[[0, 0] for _ in range(10001)] for _ in range(n)]
+        for costSum in range(10001):
+            dp[0][costSum][0] = 0
+            dp[0][costSum][1] = memories[0] if costs[0] <= costSum else 0
+
+        for i in range(1, n):
+            for costSum in range(10001):
+                #if state == 0
+                dp[i][costSum][0] = max(dp[i-1][costSum][0], dp[i-1][costSum][1])
+
+                #if state == 1
+                if 0 <= costSum-costs[i]:
+                    dp[i][costSum][1] = max(dp[i-1][costSum-costs[i]][0]+memories[i],
+                                            dp[i-1][costSum-costs[i]][1]+memories[i])
+
+        #top-down 점화식
+        # def dp(i, costSum, state):
+        #     if i == 0:
+        #         if state == 0:
+        #             return 0
+        #         return memories[0]
+        #
+        #     if state == 0:
+        #         return max(dp(i-1, costSum, 0), dp(i-1, costSum, 1))
+        #     return max(dp(i - 1, costSum - costs[i], 0) + memories[i],
+        #                dp(i - 1, costSum - costs[i], 1) + memories[i])
+
         minCost = float('inf')
-        while left!=n-1:
-            if sum < k:
-                right = right+1 if right!=n-1 else right
-                sum = sum+memories[right] if right!=n-1 else sum
-                cost = cost + costs[right] if right != n - 1 else cost
-            else:
-                minCost = min(minCost, cost)
-                sum = sum - memories[left] if (left!=n-1 and left < right) else sum
-                cost = cost - costs[left] if (left!=n-1 and left < right) else cost
-                left = left+1 if (left!=n-1 and left < right) else left
-
-            if right == n-1:
-                while True:
-                    if k <= sum and left<=n-1:
-                        minCost = min(minCost, cost)
-                        sum = sum - memories[left]
-                        cost = cost - costs[left]
-                    else:
-                        left = n-1
-                        break
-
-
+        for i in range(n):
+            for costSum in range(10001):
+                if k <= dp[i][costSum][0]:
+                    minCost = min(minCost, costSum)
+                if k <= dp[i][costSum][1]:
+                    minCost = min(minCost, costSum)
         return minCost
-
 
 
 instance = Solution()
