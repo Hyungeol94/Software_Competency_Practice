9bdb0571a08e59b9f7f0bfeedb126bbab5d6954e
Hyungeol Lee <ekfrhkquf12@naver.com>
2024-03-08 21:30:47 +0900

    BOJ 'LCS 2' 2nd trial- timeout

--- 2024/20240307 LCS 2 /소스.py
+++ 2024/20240307 LCS 2 /소스.py
@@ -8,18 +8,28 @@ class Solution():
         dp = [[0]*(m) for _ in range(n)]
         path = [[0]*(m) for _ in range(n)]
         dp[0][0] = 1 if string1[0] == string2[0] else 0
+        path[0][0] = 3 if string1[0] == string2[0] else 0
+
         for j in range(1, m):
             dp[0][j] = 1 if string1[0] == string2[j] or dp[0][j-1] else 0
-            path[0][j] = 3 if string1[0] == string2[j] or dp[0][j-1] else 0
+            if string1[0] == string2[j]:
+                path[0][j] = 3
+            if not string1[0] == string2[j] and dp[0][j-1]:
+                path[0][j] = 2
 
         for i in range(1, n):
             dp[i][0] = 1 if string1[i] == string2[0] or dp[i-1][0] else 0
-            path[i][0] = 3 if string1[i] == string2[0] or dp[i-1][0] else 0
+            if string1[i] == string2[0]:
+                path[i][0] = 3
+            if not string1[i] == string2[0] and dp[i-1][0]:
+                path[i][0] = 1
 
+        seq_len = 0
         for i in range(1, n):
             for j in range(1, m):
                 if string1[i] == string2[j]:
                     dp[i][j] = dp[i-1][j-1]+1
+                    seq_len = max(seq_len, dp[i][j])
                     path[i][j] = 3
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
@@ -28,49 +38,32 @@ class Solution():
                     else:
                         path[i][j] = 2
 
-        seq_len = 0
-        point = [0, 0]
         for i in range(n):
-            seq_len = max(seq_len, max(dp[i]))
+            seq_len = max(seq_len, dp[i][0])
+
+        for j in range(m):
+            seq_len = max(seq_len, dp[0][j])
 
         if not seq_len:
             print(0)
             return
 
-        flag = False
-        for i in range(n):
-            if flag:
-                break
-            for j in range(m):
-                if dp[i][j] == seq_len:
-                    point = [i, j]
-                    flag = True
-                    break
-
-
-        if point[0] == 0 or point[1] == 0:
-            seq = string1[0] if point[0] == 0 else string2[0]
-            print(1)
-            print(seq)
-            return
-
-        # seq = string1[point[0] - 1]
+        point = [n-1, m-1]
         seq = ''
         while True:
             x, y = point
-            if x < 0 or y < 0:
-                break
             if path[x][y] == 1:
                 point = [x-1, y]
-                continue
+
             if path[x][y] == 2:
                 point = [x, y-1]
-                continue
+
             if path[x][y] == 3:
                 seq = string1[x]+seq
                 point = [x-1, y-1]
-                continue
-            else:
+
+            x, y = point
+            if x < 0 or y < 0:
                 break
 
         print(seq_len)
@@ -78,16 +71,6 @@ class Solution():
         return
 
 
-        # @lru_cache(maxsize=None)
-        # def dp(i, j):
-        #     if i == n or j == n:
-        #         return 0
-        #     else:
-        #         if string1[i] == string2[j]:
-        #             return 1+dp(i+1, j+1)
-        #         else:
-        #             return max(dp(i+1, j), dp(i, j+1))
-        # return dp(0, 0)
 instance = Solution()
 string1 = input().strip()
 string2 = input().strip()
