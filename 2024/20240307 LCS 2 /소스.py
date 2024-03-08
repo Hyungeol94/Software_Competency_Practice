81c4cc94b557c6120ade5d63c8734d95de49f16c
Hyungeol Lee <ekfrhkquf12@naver.com>
2024-03-08 22:01:26 +0900

    BOJ 'LCS 2' 3rd trial-success

--- 2024/20240307 LCS 2 /소스.py
+++ 2024/20240307 LCS 2 /소스.py
@@ -5,71 +5,37 @@ class Solution():
     def longestCommonSubsequence(self, string1, string2):
         n = len(string1)
         m = len(string2)
-        dp = [[0]*(m) for _ in range(n)]
-        path = [[0]*(m) for _ in range(n)]
-        dp[0][0] = 1 if string1[0] == string2[0] else 0
-        path[0][0] = 3 if string1[0] == string2[0] else 0
+        dp = [[''] * m for _ in range(n)]
+        dp[0][0] = string1[0] if string1[0] == string2[0] else ''
 
         for j in range(1, m):
-            dp[0][j] = 1 if string1[0] == string2[j] or dp[0][j-1] else 0
             if string1[0] == string2[j]:
-                path[0][j] = 3
-            if not string1[0] == string2[j] and dp[0][j-1]:
-                path[0][j] = 2
+                dp[0][j] = string1[0]
+            if dp[0][j - 1] != '' and string1[0] != string2[j]:
+                dp[0][j] = dp[0][j - 1]
+
 
         for i in range(1, n):
-            dp[i][0] = 1 if string1[i] == string2[0] or dp[i-1][0] else 0
             if string1[i] == string2[0]:
-                path[i][0] = 3
-            if not string1[i] == string2[0] and dp[i-1][0]:
-                path[i][0] = 1
+                dp[i][0] = string1[i]
+            if dp[i-1][0] != '' and string1[i] != string2[0]:
+                dp[i][0] = dp[i-1][0]
 
-        seq_len = 0
+        seq = ''
         for i in range(1, n):
             for j in range(1, m):
                 if string1[i] == string2[j]:
-                    dp[i][j] = dp[i-1][j-1]+1
-                    seq_len = max(seq_len, dp[i][j])
-                    path[i][j] = 3
+                    dp[i][j] = dp[i - 1][j - 1] + string1[i]
+                    seq = dp[i][j] if len(dp[i][j]) > len(seq) else seq
                 else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-                    if dp[i][j] == dp[i-1][j]:
-                        path[i][j] = 1
-                    else:
-                        path[i][j] = 2
+                    dp[i][j] = dp[i - 1][j] if len(dp[i - 1][j]) > len(dp[i][j - 1]) else dp[i][j - 1]
 
-        for i in range(n):
-            seq_len = max(seq_len, dp[i][0])
-
-        for j in range(m):
-            seq_len = max(seq_len, dp[0][j])
-
-        if not seq_len:
+        if seq == '':
             print(0)
             return
-
-        point = [n-1, m-1]
-        seq = ''
-        while True:
-            x, y = point
-            if path[x][y] == 1:
-                point = [x-1, y]
-
-            if path[x][y] == 2:
-                point = [x, y-1]
-
-            if path[x][y] == 3:
-                seq = string1[x]+seq
-                point = [x-1, y-1]
-
-            x, y = point
-            if x < 0 or y < 0:
-                break
-
-        print(seq_len)
+        print(len(seq))
         print(seq)
         return
-
 
 instance = Solution()
 string1 = input().strip()
