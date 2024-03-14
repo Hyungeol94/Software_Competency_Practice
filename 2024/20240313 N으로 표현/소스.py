f0173b86e2590b854de6424e0b8271272077f2e9
Hyungeol Lee <ekfrhkquf12@naver.com>
2024-03-14 13:42:41 +0900

    BOJ 'N으로 표현' 풀이입니다.

--- 2024/20240313 N으로 표현/소스.py
+++ 2024/20240313 N으로 표현/소스.py
@@ -1,3 +1,4 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/42895
 from functools import lru_cache
 
 
@@ -5,6 +6,7 @@ def solution(N, number):
     nums = {}
     i = N
     count = 1
+    nums[1] = 2 if number!= 1 else 1
     while count <= 8:
         nums[i] = count
         i = int(str(i) + str(N))
@@ -17,15 +19,11 @@ def solution(N, number):
 
         if i == number:
             return depth
-        
+
         candidates = [float('inf')]
         for num, count in list(nums.items()):
-            if 0 <= i-num:
-                candidates.append(dp(i-num, depth+count))
-
-            if (i % num) == 0:
-                candidates.append(dp(i//num, depth+count))
-
+            candidates.append(dp(i-num, depth+count))
+            candidates.append(dp(i//num, depth+count))
             candidates.append(dp(i*num, depth+count))
             candidates.append(dp(i+num, depth+count))
         return min(candidates)
@@ -34,4 +32,4 @@ def solution(N, number):
     for num, count in list(nums.items()):
         candidates.append(dp(num, count))
 
-    return min(candidates) if min(candidates)!=float('inf') else 0
+    return min(candidates) if min(candidates)!=float('inf') else -1
