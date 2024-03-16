4172359e88b0fae79f0d579421b47bb8f88b4819
Hyungeol Lee <ekfrhkquf12@naver.com>
2024-03-16 22:11:38 +0900

    LeetCode 'Contiguous Array' 2nd  trial- fail

--- 2024/20240316 Contiguous Array/소스.py
+++ 2024/20240316 Contiguous Array/소스.py
@@ -2,24 +2,29 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [[[0]*n for _ in range(n)] for _ in [0, 1]]
-        dp[0][0][0] = 1 if nums[0] == 0 else 0
-        dp[1][0][0] = 1 if nums[0] == 1 else 0
+        zero_acc, one_acc = [0]*n, [0]*n
+        zero_acc[0] = 1 if nums[0] == 0 else 0
+        one_acc[0] = 1 if nums[0] == 1 else 0
 
         for i in range(1, n):
-            dp[0][0][i] = dp[0][0][i-1]+1 if nums[i] == 0 else dp[0][0][i-1]
-            dp[1][0][i] = dp[1][0][i-1]+1 if nums[i] == 1 else dp[1][0][i-1]
+            zero_acc[i] = zero_acc[i-1]+1 if nums[i] == 0 else zero_acc[i-1]
+            one_acc[i] = one_acc[i-1]+1 if nums[i] == 1 else one_acc[i-1]
         
-        for i in range(1, n):
-            for j in range(i, n): #i에서 j까지
-                dp[0][i][j] = dp[0][0][j]-dp[0][0][i-1]
-                dp[1][i][j] = dp[1][0][j]-dp[1][0][i-1]
-        
+        for i in reversed(range(n)):
+            if zero_acc[i] == one_acc[i]:
+                return i+1
+
         maxLen = 0
-        for i in range(n):
-            for j in range(i, n):
-                if dp[0][i][j] == dp[1][i][j]:
-                    maxLen = max(maxLen, j-i+1)
+        for i in range(1, n-1):
+            for j in range(i+1, n):
+                #i에서 j까지
+                if zero_acc[j]-zero_acc[i-1] == one_acc[j]-one_acc[i-1]:
+                    maxLen = max(maxLen,j-i+1)
         return maxLen
+
+                    
+
+        
+       
 
         
