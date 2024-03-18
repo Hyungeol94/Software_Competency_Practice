ad474c7c153b700d3e5e8c9d7958a3777fa12b4a
Hyungeol Lee <ekfrhkquf12@naver.com>
2024-03-18 19:03:55 +0900

    LeetCode 'Insert Interval' 풀이입니다.

--- 2024/20240318 Insert Interval/소스.py
+++ 2024/20240318 Insert Interval/소스.py
@@ -1,21 +1,25 @@
 #https://leetcode.com/problems/insert-interval/description/?envType=daily-question&envId=2024-03-17
 from collections import deque
-from collections import deque
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         #interval이 overlap하면 merge하기
         #newInterval과 overlap하는 것을 찾아내기
         arr = []
         intervals = deque(intervals)
-        
+        newInterval_left, newInterval_right = newInterval 
+
         if intervals and newInterval[1] < intervals[0][0]:
             arr.append(newInterval)
+            newInterval = []
 
-        newInterval_left, newInterval_right = newInterval
         while intervals:
             start, end = intervals[0]
-            if end < newInterval_left or newInterval_right < start:
+            if end < newInterval_left or newInterval_right < start:                 
+                if newInterval_right < start and newInterval:
+                    arr.append(newInterval)
+                    newInterval = []
                 arr.append(intervals.popleft())
+
             else:
                 if start <= newInterval[1]:
                     new_start = min(newInterval[0], start)
@@ -31,9 +35,9 @@ class Solution:
                         if intervals: 
                             start, end = intervals[0]
                     arr.append([new_start, new_end])
+                newInterval = []
 
-            
-
+  
         if not arr: 
             arr.append(newInterval)
         
