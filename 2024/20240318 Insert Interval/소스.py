891f6d249854ba5c504c3ba1d331a0cedb74c541
Hyungeol Lee <ekfrhkquf12@naver.com>
2024-03-18 14:11:09 +0900

    LeetCode 'Insert Interval' 2nd trial-fail

--- 2024/20240318 Insert Interval/소스.py
+++ 2024/20240318 Insert Interval/소스.py
@@ -1,3 +1,5 @@
+#https://leetcode.com/problems/insert-interval/description/?envType=daily-question&envId=2024-03-17
+from collections import deque
 from collections import deque
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
@@ -5,13 +7,18 @@ class Solution:
         #newInterval과 overlap하는 것을 찾아내기
         arr = []
         intervals = deque(intervals)
+        
+        if intervals and newInterval[1] < intervals[0][0]:
+            arr.append(newInterval)
+
+        newInterval_left, newInterval_right = newInterval
         while intervals:
             start, end = intervals[0]
-            if end < newInterval[0] or newInterval[1] < start:
+            if end < newInterval_left or newInterval_right < start:
                 arr.append(intervals.popleft())
             else:
-                if start < newInterval[1]:
-                    new_start = start
+                if start <= newInterval[1]:
+                    new_start = min(newInterval[0], start)
                     new_end =  0
                     while intervals and end <= newInterval[0]:
                         new_end = max(newInterval[1], end)
@@ -24,11 +31,23 @@ class Solution:
                         if intervals: 
                             start, end = intervals[0]
                     arr.append([new_start, new_end])
+
+            
+
         if not arr: 
             arr.append(newInterval)
+        
+        if newInterval and arr[-1][1] < newInterval[0]:
+            arr.append(newInterval)
+
         return arr
                     
                     
+                    
+
+                
+                
+        
                     
 
                 
