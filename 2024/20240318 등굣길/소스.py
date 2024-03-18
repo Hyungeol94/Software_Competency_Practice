6a41a9e28f6a550a263ad967109803523bdf8ecd
Hyungeol Lee <ekfrhkquf12@naver.com>
2024-03-18 20:16:49 +0900

    프로그래머스 '등굣길' 풀이입니다.

--- 2024/20240318 등굣길/소스.py
+++ 2024/20240318 등굣길/소스.py
@@ -1,16 +1,14 @@
-#https://school.programmers.co.kr/learn/courses/30/lessons/42898
-
 def solution(m, n, puddles):
     dp = [[0]*m for _ in range(n)]
     dp[0][0] = 1
     for i in range(1, m):
-        dp[0][i] = 1 if dp[0][i-1] == 1 and ([1, i+1] not in puddles) else 0
+        dp[0][i] = 1 if dp[0][i-1] == 1 and ([i+1, 1] not in puddles) else 0
     
     for i in range(1, n):
-        dp[i][0] = 1 if dp[i-1][0] == 1 and ([i+1, 1] not in puddles) else 0
+        dp[i][0] = 1 if dp[i-1][0] == 1 and ([1, i+1] not in puddles) else 0
             
     for i in range(1, n):
         for j in range(1, m):
-            dp[i][j] = dp[i-1][j]+dp[i][j-1] if [i+1, j+1] not in puddles else 0
+            dp[i][j] = dp[i-1][j]+dp[i][j-1] if [j+1, i+1] not in puddles else 0
     
     return dp[n-1][m-1] % 1000000007
