3413875e309d91e25c6af8f1a4f860811fd8587b
Hyungeol Lee <ekfrhkquf12@naver.com>
2024-03-25 10:33:42 +0900

    LeetCode 'Find All Duplicates in an Array' 2nd approach

--- 2024/20240325 Find All Duplicates in an Array/소스.py
+++ 2024/20240325 Find All Duplicates in an Array/소스.py
@@ -1,10 +1,11 @@
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
         answer = []
-        check = 0
+        numSet = set([0])
         for num in nums:
-            if not check & (1 << num):
-                check = check | (1 << num)
+            if num not in numSet:
+                numSet.add(num)
             else:
                 answer.append(num)
+
         return answer
