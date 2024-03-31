61548f091176876b888e0fb2da1ab451f1e1bb63
Hyungeol Lee <ekfrhkquf12@naver.com>
2024-03-31 22:43:08 +0900

    LeetCode 'Count Subarrays With Fixed Bounds' 2nd trial-success

--- 2024/20240331 Count Subarrays With Fixed Bounds/소스.py
+++ 2024/20240331 Count Subarrays With Fixed Bounds/소스.py
@@ -1,35 +1,53 @@
 from copy import deepcopy
 class Solution:
     def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
-        freqDist = {}
+
+        # freqDist = {}
+        numSet = set()
         left = 0
-        right = 0
-       
+        right = 0       
         count = 0
+      
+        sliding_start = 0
+        minIndex = -1
+        maxIndex = -1
+
         while right<len(nums):
-            freqDist[nums[right]] = 1 if nums[right] not in freqDist else freqDist[nums[right]]+1
-            if minK not in freqDist or maxK not in freqDist:
-                if minK <= min(freqDist) and max(freqDist) <= maxK:
-                    right += 1
+            numSet.add(nums[right])
+            if nums[right] < minK or maxK < nums[right]:
+                left = right+1
+                right = left
+                # minIndex = -1
+                # maxIndex = -1
+                sliding_start = left
+                numSet = set()
+                continue
+
+            if nums[right] == minK:
+                minIndex = right
+                #여기에서 window_start를 업데이트!
+                #sliding_start와 right 사이에 있는 minK와 maxK의 인덱스 중 더 작은 곳의 위치로 업데이트
+                sliding_start = min(minIndex, maxIndex)
+            if nums[right] == maxK:
+                maxIndex = right
+                sliding_start = min(minIndex, maxIndex)
+            
+            if minK not in numSet or maxK not in numSet:
+                sliding_start += 1
+                if minIndex < left or maxIndex < left:
+                    right += 1                    
                     continue
 
-            if min(freqDist) == minK and max(freqDist) == maxK:
-                minKcount = freqDist[minK]
-                maxKcount = freqDist[maxK]
-                temp = left
-                while minKcount != 0 and maxKcount != 0 and temp <= right:
-                    count += 1
-                    if nums[temp] == minK:
-                        minKcount -= 1
-                    if nums[temp] == maxK:
-                        maxKcount -= 1
-                    temp += 1
+
+            if min(numSet) == minK and max(numSet) == maxK:
+                count += sliding_start-left+1
                 right += 1
                 continue
 
             else:
                 left = right+1
                 right = left
-                freqDist = {}
+                sliding_start = left
+                numSet = set()
         
         return count
