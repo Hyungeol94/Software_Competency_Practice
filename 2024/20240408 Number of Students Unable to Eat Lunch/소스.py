1ec9a7a3724a71fdba62b73f19504013ec29e57d
Lyosha's MacBook <lyoshasmacbook@10-141-43-16.0103.pos.kr.iosda.org>
2024-04-08 10:34:00 +0900

    LeetCode 'Number of Students Unable to Eat Lunch' 1st trial-fail

---
+++ 2024/20240408 Number of Students Unable to Eat Lunch/소스.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        students = deque(students)
+        sandwiches.reverse()
+        while students and sandwiches:
+            if students[0] == sandwiches[-1]:
+                students.popleft()
+                sandwiches.pop()
+            else:
+                count = 0
+                while count!=len(students):
+                    students.rotate(-1)
+                    count += 1
+                    if students[0] == sandwiches[-1]:
+                        students.popleft()
+                        sandwiches.pop()
+                        break
+                if count == len(students):
+                    return len(students)
+        return 0
