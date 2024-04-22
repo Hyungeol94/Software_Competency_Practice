7d61a06a436162c6604133638a465e310c20b161
Lyosha's MacBook <lyoshasmacbook@10-141-41-72.0101.pos.kr.iosda.org>
2024-04-22 19:45:12 +0900

    BOJ '강의실 배정' 2nd trial with greedy approach-success

--- 2024/20240304 강의실 배정/소스.py
+++ 2024/20240304 강의실 배정/소스.py
@@ -1,4 +1,6 @@
 from collections import deque
+import heapq
+
 class Solution:
     def __init__(self):
         self.N = int(input())
@@ -10,37 +12,33 @@ class Solution:
         self.times.sort(key = lambda a:a[0])
         self.times = deque(self.times)
 
-    def minClassroom(self):
-        maxNum = -float('inf')
-        i = 0
-        #각 타임마다 겹치는 게 몇 개인지만 트래킹하기
-        if len(self.times) == 1:
-            return 1
-        left = 0 #top index
-        right = 1 #bottom index
-        #sliding window
-        top_s, top_t = self.times[left]
-        bottom_s, bottom_t = self.times[right]
-        command = "expand"
-        count = 1 if top_t > bottom_s else 0
-        maxNum = 1
-        while right != len(self.times):
-            top_s, top_t = self.times[left]
-            bottom_s, bottom_t = self.times[right]
-            if top_t > bottom_s:
-                count += 1
-                maxNum = max(maxNum, count)
-                right += 1
-            else:
-                left += 1
-                count = count-1 if count > 1 else 1
-                if left == right:
-                    right += 1
 
-        print(maxNum)
+    def minClassroom(self):
+        syncClasses = []
+        heapq.heapify(syncClasses)
+        start, end = self.times.popleft()
+        heapq.heappush(syncClasses, [end, start])
+        maxNum = 1
+        
+        while self.times:
+            curr_start, curr_end = self.times.popleft()
+            if not syncClasses:
+                heapq.heappush(syncClasses, [curr_end, curr_start])
+            top_end, top_start = syncClasses[0]
+            if not curr_start < top_end:
+                while syncClasses:
+                    top_end, top_start = syncClasses[0]
+                    if curr_start < top_end:
+                        break
+                    heapq.heappop(syncClasses)
+
+            heapq.heappush(syncClasses, [curr_end, curr_start])
+            maxNum = max(maxNum, len(syncClasses))
+        return maxNum 
+
 
 instance = Solution()
-instance.minClassroom()
+print(instance.minClassroom())
 
 
 
