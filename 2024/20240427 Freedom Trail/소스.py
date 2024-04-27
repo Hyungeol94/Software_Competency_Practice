00a89c16a187a0b580b305b67d5ea82485793bc2
Hyungeol Lee <ekfrhkquf12@naver.com>
2024-04-28 00:10:05 +0900

    LeetCode 'Freedom Trail' 2nd trial - success

--- 2024/20240427 Freedom Trail/소스.py
+++ 2024/20240427 Freedom Trail/소스.py
@@ -4,15 +4,17 @@ from functools import cache
 
 class Solution:
     def findRotateSteps(self, ring: str, key: str) -> int:
-        @lru_cache(maxsize = None)
+        @cache
         def dp(i, number):
             #모든 가능한 상황을 구하기
             #그 상황 중에서 제일 적게 들었던 step 수를 더하기
-            current_ring = copy.deepcopy(deque(ring))
-            current_ring.rotate(number)
-
+            # current_ring = copy.deepcopy(deque(ring))
+            # current_ring.rotate(number)
+            current_index = number
+            current_index %= len(ring)
             #동일할 때
-            if key[i] == current_ring[0]:
+            # if key[i] == current_ring[0]:
+            if key[i] == ring[current_index]:
                 if i == len(key)-1:
                     return 1
                 else:
@@ -23,17 +25,24 @@ class Solution:
             steps = 0
             candidates = []
             for count in range(1, math.ceil(len(ring)/2)):
-                current_ring.rotate(1)
-                if key[i] == current_ring[0]:
+                # current_ring.rotate(1)
+                # if key[i] == current_ring[0]:
+                current_index += 1
+                current_index %= len(ring)
+                if key[i] == ring[current_index]:
                     candidates.append([count, count])
                         
-            current_ring = copy.deepcopy(deque(ring))
-            current_ring.rotate(number)
+            # current_ring = copy.deepcopy(deque(ring))
+            # current_ring.rotate(number)
+            current_index = number
             count = 0
             for _ in range(math.floor(len(ring)/2), len(ring)):
-                current_ring.rotate(-1)
+                # current_ring.rotate(-1)
+                current_index -= 1
+                current_index %= len(ring)
                 count += 1
-                if key[i] == current_ring[0]:
+                # if key[i] == current_ring[0]:
+                if key[i] == ring[current_index]:
                     candidates.append([count, -count])
             
             if i == len(key)-1:
