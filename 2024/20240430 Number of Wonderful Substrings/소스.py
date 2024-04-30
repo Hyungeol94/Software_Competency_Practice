2aa09faac03c0a274c3405a5b2e2205a25e5a1e3
Hyungeol Lee <ekfrhkquf12@naver.com>
2024-04-30 23:16:33 +0900

    LeetCode 'Number of Wonderful Substrings' 2nd trial- time limit exceed

--- 2024/20240430 Number of Wonderful Substrings/소스.py
+++ 2024/20240430 Number of Wonderful Substrings/소스.py
@@ -1,16 +1,26 @@
 from collections import defaultdict
+from itertools import combinations
+
 class Solution:
+    def isWonderful(self, number):
+        if number.bit_count() <= 1:
+            return True
+        return False
+
+
     def wonderfulSubstrings(self, word: str) -> int:
         #10억번의 연산이라면.. 아슬아슬할지도??
         count = 0
-        for i, _ in enumerate(word):
-            charSet = set()
-            for j, c in enumerate(word[i:]):
-                if c in charSet:
-                    charSet.remove(c)
-                else :
-                    charSet.add(c)
-                if len(charSet) < 2:
-                    count += 1
-        
+        bitMasks = [0]*len(word)
+        charSet = set()
+        bit = 0
+        for i, c in enumerate(word):
+            bit = bit ^ (1 << ord(c)-ord('a'))
+            bitMasks[i] = bit           
+            count = count+1 if self.isWonderful(bit) else count
+
+            for j in range(i):
+                temp = bitMasks[j]^bitMasks[i]
+                count = count+1 if self.isWonderful(temp) else count
+
         return count
