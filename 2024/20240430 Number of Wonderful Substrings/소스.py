104507f3d23b04182d2efbb281da2483033dd3e5
Hyungeol Lee <ekfrhkquf12@naver.com>
2024-05-02 11:47:14 +0900

    LeetCode 'Number of Wonderful Substrings' Editorial 참고하여 풀이

--- 2024/20240430 Number of Wonderful Substrings/소스.py
+++ 2024/20240430 Number of Wonderful Substrings/소스.py
@@ -1,26 +1,24 @@
 from collections import defaultdict
-from itertools import combinations
 
 class Solution:
-    def isWonderful(self, number):
-        if number.bit_count() <= 1:
-            return True
-        return False
-
-
     def wonderfulSubstrings(self, word: str) -> int:
         #10억번의 연산이라면.. 아슬아슬할지도??
         count = 0
         bitMasks = [0]*len(word)
-        charSet = set()
+        maskFreqDist = defaultdict(int)
         bit = 0
+
+        #홀수가 하나도 없을 때
         for i, c in enumerate(word):
             bit = bit ^ (1 << ord(c)-ord('a'))
-            bitMasks[i] = bit           
-            count = count+1 if self.isWonderful(bit) else count
-
-            for j in range(i):
-                temp = bitMasks[j]^bitMasks[i]
-                count = count+1 if self.isWonderful(temp) else count
+            count = count+1 if not bit else count
+            count += maskFreqDist[bit]
+            maskFreqDist[bit] += 1
+            
+            #홀수가 하나만 있을 때
+            for c in range(0, 10):
+                #현재 bitMask에서 c에 해당하는 비트를 flip했을 때의 결과 : bit^(1<<c)
+                count = count+1 if not bit^(1 << c) else count
+                count += maskFreqDist[bit^(1 << c)]
 
         return count
