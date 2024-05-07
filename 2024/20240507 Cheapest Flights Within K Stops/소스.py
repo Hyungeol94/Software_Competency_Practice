77a0eeec8469ff2932246d0ba54d8bf8f6fa678e
Lyosha's MacBook <lyoshasmacbook@10-141-56-234.0200.pos.kr.iosda.org>
2024-05-07 12:26:08 +0900

    LeetCode 'Cheapest Flights Within K Stops' 2nd approach using Bellman-Ford algorithm - success

--- 2024/20240507 Cheapest Flights Within K Stops/소스.py
+++ 2024/20240507 Cheapest Flights Within K Stops/소스.py
@@ -1,31 +1,26 @@
 from collections import defaultdict
 import heapq
+import copy
 
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         adjList = defaultdict(list)
         prices = [[float('inf')]*n for _ in range(n)]
         limit = k
-        minCost = float('inf')
+        prevCosts = [float('inf')]*n
 
-        for k, v, price in flights:
-            adjList[k].append(v)
-            prices[k][v] = price
+        for departure, arrival, price in flights:
+            adjList[departure].append(arrival)
+            prices[departure][arrival] = price
+            if departure == src:
+                prevCosts[arrival] = price
 
-        myqueue = []
-        heapq.heapify(myqueue)
-        for node in adjList[src]:
-            heapq.heappush(myqueue, [prices[src][node], 0, node])
-        
-        while myqueue:
-            cost, stops, current = heapq.heappop(myqueue)
-            if current == dst:
-                return cost
+        for _ in range(k):
+            newCosts = copy.deepcopy(prevCosts)
+            for node in range(n):
+                for nextNode in adjList[node]:
+                    if prevCosts[node]+prices[node][nextNode] <= prevCosts[nextNode]:
+                        newCosts[nextNode] = min(newCosts[nextNode], prevCosts[node]+prices[node][nextNode])
+            prevCosts = copy.deepcopy(newCosts)
 
-            if stops == limit:
-                continue
-            
-            for node in adjList[current]:            
-                heapq.heappush(myqueue, [cost + prices[current][node], stops+1, node])
-
-        return -1
+        return prevCosts[dst] if prevCosts[dst]!=float('inf') else -1
