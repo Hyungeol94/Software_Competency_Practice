ec77e8818ee96aa77e326fb024b571267f592e9c
Hyungeol Lee <ekfrhkquf12@naver.com>
2024-05-08 22:15:37 +0900

    LeetCode 'Cheapest Flights Within K Stops' modified 1st approach using Dijkstra's algorithm- success

--- 2024/20240507 Cheapest Flights Within K Stops/소스.py
+++ 2024/20240507 Cheapest Flights Within K Stops/소스.py
@@ -24,3 +24,35 @@ class Solution:
             prevCosts = copy.deepcopy(newCosts)
 
         return prevCosts[dst] if prevCosts[dst]!=float('inf') else -1
+
+    def findCheapestPrice2(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        adjList = defaultdict(list)
+        prices = [[float('inf')]*n for _ in range(n)]
+        limit = k
+        stopRecords = [float('inf')]*n
+        stopRecords[src] = 0
+
+        for k, v, price in flights:
+            adjList[k].append(v)
+            prices[k][v] = price
+
+        myqueue = []
+        heapq.heapify(myqueue)
+        heapq.heappush(myqueue, [0, -1, src])
+        
+        while myqueue:
+            cost, stops, current = heapq.heappop(myqueue)
+            if current == dst:
+                return cost
+
+            if stops == limit or stopRecords[current] < stops:
+                continue
+            
+            stopRecords[current] = stops
+            
+            for node in adjList[current]:            
+                heapq.heappush(myqueue, [cost + prices[current][node], stops+1, node])
+
+
+        return -1
+
