1f0318971730042165151d1baf9af82d4c10df20
Lyosha's MacBook <lyoshasmacbook@Lyoshas-MacBook.local>
2024-06-11 14:08:26 +0900

    프로그래머스 '주사위 고르기' 해설 참고하여 풀이

--- 2024/20240611 주사위 고르기/소스.py
+++ 2024/20240611 주사위 고르기/소스.py
@@ -1,7 +1,5 @@
-#주사위 굴리기
-#A의 주사위를 굴린 결과만 시뮬레이션해 6**(n/2) 가지에 대해 주사위 눈의 합을 저장
-#B에 대해서도 마찬가지
 import itertools
+import bisect
 
 class Solution:
     def __init__(self, dice):
@@ -9,64 +7,50 @@ class Solution:
         self.n = len(dice)
         self.A_choice_indices = []
         self.B_choice_indices = []
-        #A에 대한 모든 가능한 조합의 합(저장)
         self.A_sums = []
-        #B에 대한 모든 가능한 조합의 합(저장)
         self.B_sums = []
 
     def maximumWinningProbability(self):
-        indices = [i for i in range(self.n)]
-        choice = [0, 0]
+        indices = list(range(self.n))
+        choice = []
         max_winning_count = -1
-        for A_choice_indices in itertools.combinations(indices, self.n//2):
+
+        for A_choice_indices in itertools.combinations(indices, self.n // 2):
             self.A_choice_indices = A_choice_indices
-            self.B_choice_indices = tuple(set(range(self.n))-set(A_choice_indices))
-            self.A_dices = [self.dice[i] for i in self.A_choice_indices] #A가 선택한 주사위들
-            self.B_dices = [self.dice[i] for i in self.B_choice_indices] #B가 선택한 주사위들
+            self.B_choice_indices = tuple(set(indices) - set(A_choice_indices))
+            self.A_dices = [self.dice[i] for i in self.A_choice_indices]
+            self.B_dices = [self.dice[i] for i in self.B_choice_indices]
+            
             count = self.winning_count()
-            if max_winning_count < count:
+            if count > max_winning_count:
                 max_winning_count = count
                 choice = self.A_choice_indices
-        return list([i+1 for i in choice])
 
+        return [i + 1 for i in choice]
 
     def winning_count(self):
-        #여기서 누적합 사용하기
-        self.mystack = []
-        #6^(n/2) 가지에 대해 주사위 눈의 합을 저장    
-        self.dfs(0)
-        count = 0
-        for A_sum in self.A_sums:
-            count += len(list(filter(lambda a: A_sum > a, self.B_sums)))
-        
         self.A_sums = []
         self.B_sums = []
+        
+        self.dfs()
+
+        self.A_sums.sort()
+        self.B_sums.sort()
+
+        count = 0
+        for A_sum in self.A_sums:
+            B_index = bisect.bisect_left(self.B_sums, A_sum)
+            count += B_index
+
         return count
 
-
-    def dfs(self, depth):
-        if (depth == self.n//2):
-        #self.A_sums, self.B_sums 업데이트하기
-            A_sum = 0
-            B_sum = 0
-            for i, (A_dice, B_dice) in enumerate(zip(self.A_dices, self.B_dices)):
-                A_sum += A_dice[self.mystack[i]] #주사위 눈의 합을 저장
-                B_sum += B_dice[self.mystack[i]] #주사위 눈의 합을 저장
+    def dfs(self):
+        for indices in itertools.product(range(6), repeat=self.n // 2):
+            A_sum = sum(self.A_dices[i][index] for i, index in enumerate(indices))
+            B_sum = sum(self.B_dices[i][index] for i, index in enumerate(indices))
             self.A_sums.append(A_sum)
             self.B_sums.append(B_sum)
- 
-        else:
-            for i in range(6):
-                self.mystack.append(i)
-                self.dfs(depth+1)
-                self.mystack.pop()
-
 
 def solution(dice):
     instance = Solution(dice)
-    answer = instance.maximumWinningProbability()
-    return answer
-
-# instance = Solution(dice)
-# instance.maximumWinningProbability()
-
+    return instance.maximumWinningProbability()
