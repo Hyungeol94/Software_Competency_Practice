aab17a92f296a1c4c409e70902b26933a268c460
Hyungeol94 <ekfrhkquf12@naver.com>
2024-09-14 22:09:42 +0900

    LeetCode 'Longest Subarray With Maximum Bitwise AND' 풀이입니다.

---
+++ 2024/20240914 Longest Subarray With Maximum Bitwise AND/소스.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        max_val = max(nums)
+        i = 0
+        n = len(nums)
+        
+        max_len = 0
+
+        def update():
+            nonlocal i, max_len
+            count = 0
+            while i != len(nums) and nums[i] == max_val:
+                count += 1
+                max_len = max(max_len, count)
+                i += 1
+        
+        while i < n:
+            if nums[i] == max_val:
+                update()
+            i += 1
+        return max_len
