f27f300bedfb3780ea53c5fe3de0af72a09bc151
Hyungeol94 <ekfrhkquf12@naver.com>
2025-01-06 09:48:18 +0900

    LeetCode 'Shifting Letters II' 2nd approach based on editorial reference - success

--- 2025/20250105/소스2.py
+++ 2025/20250105/소스2.py
@@ -1,17 +1,29 @@
+#https://leetcode.com/problems/shifting-letters-ii/?envType=daily-question&envId=2025-01-05
+#Shifting Letters II
+
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
         acc = [0]*len(s)
+        n = len(s)
         for shift in shifts:
             start, end, dr = shift
-            for i in range(start, end+1):
-                acc[i] = acc[i]+1 if dr == 1 else acc[i]-1
+            if dr == 1:
+                acc[start] += 1
+                if end < n-1:
+                    acc[end+1] -= 1
+            
+            else:
+                acc[start] -= 1
+                if end < n-1:
+                    acc[end+1] += 1
         
+        curr = 0
         arr = [c for c in s]
-
-        for i, _ in enumerate(acc):
-            c = arr[i]
-            pos = ord(c) + acc[i]
-            if not ord("a") <= pos <= ord("z"):
-                pos = pos % (ord("z")+1) + ord("a")
-            arr[i] = chr(pos)
+        for i, c in enumerate(arr):
+            curr += acc[i]
+            c = chr(
+                (ord(c) - ord('a') + curr) % 26 + ord('a')
+            )
+            arr[i] = c
+        
         return "".join(arr)
