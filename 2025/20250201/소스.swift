74eb583c4dd6bce215162879ffc87a3e20a74448
Hyungeol94 <ekfrhkquf12@naver.com>
2025-02-01 12:41:44 +0900

    LeetCode 'Special Array I' 풀이입니다.

---
+++ 2025/20250201/소스.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func isArraySpecial(_ nums: [Int]) -> Bool {
+        let n = nums.count
+        for i in 0..<n {
+            if i == n-1 { 
+                break
+            }
+        if nums[i] % 2 == nums[i+1] % 2 {
+            return false
+        }
+        }
+    return true
+}
+}
