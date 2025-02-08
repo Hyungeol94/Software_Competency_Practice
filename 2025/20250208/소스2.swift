5d92179b6d714ebc6424ac0d077053ce5afc2ee0
Hyungeol94 <ekfrhkquf12@naver.com>
2025-02-08 21:57:29 +0900

    LeetCode 'Guess Number Higher or Lower' 풀이입니다.

---
+++ 2025/20250208/소스2.swift
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/guess-number-higher-or-lower/
+
+/** 
+ * Forward declaration of guess API.
+ * @param  num -> your guess number
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0 
+ * func guess(_ num: Int) -> Int 
+ */
+
+class Solution : GuessGame {
+    func guessNumber(_ n: Int) -> Int {
+        var left = 0
+        var right = n
+        while true {
+            let middle = (left + right) / 2 
+            let guess = self.guess(middle)
+            if guess == -1 { 
+                right = middle - 1
+            } else if guess == 1 {
+                left = middle + 1
+            } else { 
+                return middle
+            }
+        }
+    }
+}
