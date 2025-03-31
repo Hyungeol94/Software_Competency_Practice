3705d101aeaf9cd3c953e98a362ceaad1ec243b5
Hyungeol94 <ekfrhkquf12@naver.com>
2025-03-31 11:50:11 +0900

    LeetCode 'Put Marbles in Bags' O(nlogn) 시간복잡도로 풀이

--- 2025/20250331/소스.py
+++ 2025/20250331/소스.py
@@ -4,6 +4,23 @@
 from functools import cache
 class Solution:
     def putMarbles(self, weights: List[int], k: int) -> int:
+        #합이 제일 큰 n쌍을 구하기
+        #n 조합 중에서 가장 합이 큰 경우를 구하기
+        #-> 정렬? 
+        if k == 1:
+            return 0
+
+        arr = []
+        n = len(weights)
+        for i in range(n-1):
+            arr.append(weights[i]+weights[i+1])
+        
+        arr.sort()#정렬
+        #그 중에서 k-1개 고르기
+        return sum(arr[-k+1:]) - sum(arr[:k-1])
+
+
+    def putMarbles_TLE(self, weights: List[int], k: int) -> int:
         n = len(weights)
         @cache #일단 최댓값 계산 먼저
         def max_dp(i, k):
