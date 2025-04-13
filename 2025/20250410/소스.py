8924987fb903c3e559cd425852a292dd5109041e
Hyungeol94 <ekfrhkquf12@naver.com>
2025-04-13 20:07:04 +0900

    LeetCode 'Count the Number of Powerful Integers' 2nd trial 힌트 참조 - success

--- 2025/20250410/소스.py
+++ 2025/20250410/소스.py
@@ -4,6 +4,58 @@
 from functools import cache
 
 class Solution:
+    def numberOfPowerfulInt(self, start: int, finish: int, limit: int, s: str) -> int:
+        @cache
+        def dp(x, pos, is_tight):
+            # 이미 앞에서 다 정해졌을 때
+            if pos == len(x) - len(s):
+                if is_tight == 0:
+                    return 1
+
+                else:  # tight할 때 (앞에서부터 계속 tight해 왔음)
+                    if int(s) > int(x[-len(s):]):
+                        return 0
+                    return 1
+                
+            # 마지막 자리가 아닐 때
+            else:
+                if is_tight == 1:  # 현재가 tight할 때
+                    curr = min(limit, int(x[pos]))
+                    count = 0
+                    if limit < int(x[pos]):
+                        count += dp(x, pos+1, 0)
+                    else:
+                        count += dp(x, pos + 1, 1)# 계속 tight해지는 경우
+                    if curr != 0:  # 느슨해지는 경우
+                        count += curr * dp(x, pos + 1, 0)
+                    return count
+
+                else:  # tight하지 않을 때
+                    # 0부터 limit까지 포함 가능
+                    return (limit + 1) * dp(x, pos + 1, 0)  # 계속 tight하지 않음
+
+        if len(str(finish)) <= len(s):
+            if int(s) <= int(finish):
+                return 1
+            else:
+                return 0
+
+        front_digit = int(str(finish)[0])
+        finish_count = dp(str(finish), 0, 0) if limit < front_digit else dp(str(finish), 0, 1)
+
+        if len(str(start-1)) < len(s):
+            if int(s) < int(start-1):
+                return finish_count - 1
+            else:
+                return finish_count
+
+        start_count = 0
+        front_digit = int(str(start-1)[0])
+        start_count = dp(str(start-1), 0, 0) if limit < front_digit else dp(str(start-1), 0, 1)
+
+        return finish_count - start_count
+    
+
     def numberOfPowerfulInt_MLE(self, start: int, finish: int, limit: int, s: str) -> int:
         count = 0
         if start <= int(s) <= finish:
