ef2cce1340aca7327e11acc99047b79110878b55
Hyungeol94 <ekfrhkquf12@naver.com>
2025-05-11 14:29:28 +0900

    LeetCode 'Three Consecutive Odds' 풀이입니다.

---
+++ 2025/20250511/소스.py
@@ -0,0 +1,21 @@
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        if len(arr) < 3:
+            return False
+        
+        count = sum([True if num % 2 == 1 else False for num in arr[:3]])
+        if count == 3:
+            return True
+        j = 0
+        i = 3
+        n = len(arr)
+        while i < n:
+            if arr[j] % 2 == 1:
+                count -= 1
+            if arr[i] % 2 == 1:
+                count += 1
+            if count == 3:
+                return True
+            i += 1
+            j += 1
+        return False
