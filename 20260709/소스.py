543f141a98400fe3dc413c114b81089ccd607c8f
Lyosha <ekfrhkquf12@naver.com>
2026-07-10 09:00:09 +0900

    LeetCode 'Path Existence Queries in a Graph I' 풀이입니다.
    Implement pathExistenceQueries method to determine if a path exists between given nodes in a graph based on maxDiff.

---
+++ 20260709/소스.py
@@ -0,0 +1,28 @@
+#https://leetcode.com/problems/path-existence-queries-in-a-graph-i/description/?envType=daily-question&envId=2026-07-09
+#3532. Path Existence Queries in a Graph I
+
+class Solution:
+    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
+        n = len(nums)
+        next = [-1]*n
+        left = 0
+        right = 0
+        while left < n:
+            while right < n and nums[right]-nums[left] <= maxDiff:
+                right += 1
+            next[left] = right-1
+            left += 1
+
+        @cache
+        def dp(k): #max reach를 반환
+            if next[k] == k:
+                return k
+                
+            return dp(next[k])
+
+        ans = []
+        for k, v in queries:
+            if k > v:
+                k, v = v, k
+            ans.append(True if dp(k) >= v else False)
+        return ans 
